fb19d78a0c427002f044796ee05aac3cdd032ffa
Merlin6Pedro <140712807+Merlin6Pedro@users.noreply.github.com>
2023-10-26 09:03:30 -0300

    Add files via upload

---
+++ alura21092023-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,8 @@
+function TocaSomPom () {
+document.querySelector('#som_tecla_pom').play();  
+}
+ 
+document.querySelector('.tecla_pom').onclick = TocaSomPom;
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
